e95e212b16eb844f91ee4f26766fee393593544f
rarmanu <37037124+rarmanu@users.noreply.github.com>
2018-06-21 10:07:58 +0300

    test sketches added to test sw comm
    - sw serial communication between arduino (uno/nano) and ESP8266 (12E)
    
       - one sketch for sender
          - reads inputs from a joystick
          - reads 3 pots for PID parameters (P, I, D)
          - reads data from a MPU6050 (gyro/accelorometer --> angles)
          - sends the raw values over sw serial to ESP8266
            TO DO:
            - added a tab for MPU6050 calibration but it uses a library that is not yet included (the sketch does not compile due to that)
       
       - one sketch for receiver
          - receives raw data from arduino over sw serial comunication and sends them over MQTT to a broker
          - checks the CRC or the received message and compares with the calculated CRC;
             TO DO:
             - MQQT needs to be configured and tested

---
+++ test_swSerial_read/D_mqtt.ino
@@ -0,0 +1,127 @@
+#include "D_mqtt.h"
+
+bool blinkState = false;
+
+void wifiSetup(){
+  int con_attempts = 0;
+  int i = 0;
+  delay(10);
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+     
+  WiFi.mode(WIFI_STA);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    runEvery(4000){
+      Serial.print("Connecting to "); Serial.println(ssid[i]);
+      WiFi.begin(ssid[i], password[i]);
+      Serial.println("Please wait for connection.");
+    
+      if (i < 1){ // (2 - 1)
+        i++;
+      }
+      else{
+        i = 0;
+      }
+    }
+    yield();
+  }
+    
+//////////////////////////
+/* OLD   
+  while (WiFi.status() != WL_CONNECTED) {
+    if (_debug) {
+      Serial.println(); Serial.print("Connecting to "); Serial.println(ssid1);
+    }
+    // start with ssid1
+    WiFi.begin(ssid1, password1);
+    
+    while ((con_attempts <= 5) && (WiFi.status() != WL_CONNECTED)) {
+      
+      delay(2000);
+      if (_debug) {
+        Serial.print(con_attempts); Serial.print(" ");  
+      }
+      con_attempts++; 
+    }
+    
+    if (_debug) {
+      Serial.println(); Serial.print("Connecting to "); Serial.println(ssid2);  
+    }
+    WiFi.begin(ssid2, password2);
+
+    while ((6 <= con_attempts ) && (con_attempts <= 10) && (WiFi.status() != WL_CONNECTED)) {
+      delay(2000);
+      if (_debug) {
+        Serial.print(con_attempts);   
+      }
+      con_attempts++; 
+    }
+    con_attempts = 0; // reset number of retries
+  }
+*/
+
+//  if (_debug) {
+    Serial.println("");
+    Serial.println("WiFi connected");  
+    Serial.println("my IP address: ");
+    Serial.println(WiFi.localIP());
+//  }
+}
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    blinkState = !blinkState;
+    digitalWrite(BUILTIN_LED, blinkState);   // Turn the LED on/off 
+    
+  } 
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client", mqtt_username, mqtt_password)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish(mqtt_log, "[ESP] Remote reconnected");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 1 seconds before retrying
+      delay(1000);
+    }
+    yield();
+  }
+}
+
+
+//void publishAngles(){
+//  snprintf (msg, 75, "Angles: #%ld, #%ld", yaw, pitch);
+//  Serial.print("Publish message: ");
+//  Serial.println(msg);
+//  client.publish(mqtt_rem_ang, msg);
+//}
+
+void publishCommands(){
+  snprintf (msg, 75, "#%ld", "#%ld", "#%ld", "#%ld", "#%ld", dp.FwRev, dp.LeftRight, dp.Pot_P, dp.Pot_I, dp.Pot_D);
+  Serial.print("Publish message: ");
+  Serial.println(msg);
+  client.publish(mqtt_rem_cmds, msg);
+}
---
+++ test_swSerial_send/MPU6050.ino
@@ -0,0 +1,68 @@
+#include "Arduino.h"
+#include <Wire.h>
+//   A4 - I2C SDA 
+//   A5 - I2C SCL
+
+const int MPU_addr=0x68;  // MPU6050 address
+
+void mpu_setup(){
+  // Set up MPU 6050:
+  Wire.begin();
+  #if ARDUINO >= 157
+    Wire.setClock(400000UL); // Set I2C frequency to 400kHz
+  #else
+    TWBR = ((F_CPU / 400000UL) - 16) / 2; // Set I2C frequency to 400kHz
+  #endif
+  
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  delay(2000);
+}  // END MPU6050 setup 
+
+void get_angles(double p_ang[]){
+  const double degconvert = 57.2957786; //there are like 57 degrees in a radian. 
+  double AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ; //These will be the raw data from the MPU6050.
+  int16_t AcX_calib_val=0.0 , AcY_calib_val=0.0 , AcZ_calib_val=0.0; // accelerometer calibration values
+  int16_t GyX_calib_val=0.0 , GyY_calib_val=0.0 , GyZ_calib_val=0.0; // gyro calibration values
+  double dt, roll, pitch, gyroXrate, gyroYrate;
+   
+  //Collect raw data from the sensor.
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+  AcX += AcX_calib_val; AcY += AcY_calib_val; AcZ += AcZ_calib_val;
+  GyX += GyX_calib_val; GyY += GyY_calib_val; GyZ += GyZ_calib_val;
+  
+  dt = (double)(micros() - timer) / 1000000; //This line does three things: 1) stops the timer, 2)converts the timer's output to seconds from microseconds, 3)casts the value as a double saved to "dt".
+  timer = micros(); //start the timer again so that we can calculate the next dt.
+
+  //the next two lines calculate the orientation of the accelerometer relative to the earth and convert the output of atan2 from radians to degrees
+  //We will use this data to correct any cumulative errors in the orientation that the gyroscope develops.
+  roll = atan2(AcY, AcZ) * degconvert;
+  pitch = atan2(-AcX, AcZ) * degconvert;
+
+  //The gyroscope outputs angular velocities.  To convert these velocities from the raw data to deg/second, divide by 131.  
+  //Notice, we're dividing by a double "131.0" 
+  gyroXrate = GyX/131.0;
+  gyroYrate = GyY/131.0;
+
+  //THE COMPLEMENTARY FILTER
+  //This filter calculates the angle based MOSTLY on integrating the angular velocity to an angular displacement.
+  //dt, recall, is the time between gathering data from the MPU6050.  We'll pretend that the 
+  //angular velocity has remained constant over the time dt, and multiply angular velocity by 
+  //time to get displacement.
+  //The filter then adds a small correcting factor from the accelerometer ("roll" or "pitch"), so the gyroscope knows which way is down. 
+    p_ang[0] = 0.99*(p_ang[0] + gyroXrate * dt) + 0.01*roll; // Calculate the angle using a Complimentary filter
+    p_ang[1] = 0.97*(p_ang[1] + gyroYrate * dt) + 0.03*pitch;    // Calculate the angle using a Complimentary filter
+}
